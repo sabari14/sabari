97095d72483bd9f0ef6946a67ca3f2b128e4ff92
Karthi1301 <lavanyacs94@gmail.com>
2019-07-02 09:23:05 +0530

    success

---
+++ src/org/employee/Emp.java
@@ -0,0 +1,34 @@
+package org.employee;
+
+public class Emp {
+	
+	
+		public void empId()
+		{
+			System.out.println("Emp id: 12345");
+		}
+		public void empName()
+		{
+		System.out.println("emp Name: Arul");
+		}
+		public void empDob()
+		{
+			System.out.println("DOB: 12/08/2001");
+		}
+		public void empPhone()
+		{
+			System.out.println("employee phone number: 9845321365");
+		}
+		public void emailID()
+		{
+			System.out.println("Email id: aaa@gmail.com");
+		}
+		public void address()
+		{
+			System.out.println("Address: no:2 mettu street");
+		}
+		
+	}
+		
+
+
